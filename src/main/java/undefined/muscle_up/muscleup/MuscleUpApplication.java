86be7a0de4edbb0594e0349394c684801272d5a7
Ryua <machiro119@naver.com>
2020-09-22 13:56:14 +0900

    [INIT] - project

---
+++ src/main/java/undefined/muscle_up/muscleup/MuscleUpApplication.java
@@ -0,0 +1,13 @@
+package undefined.muscle_up.muscleup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscleUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuscleUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/undefined/muscle_up/muscleup/MuscleUpApplicationTests.java
@@ -0,0 +1,13 @@
+package undefined.muscle_up.muscleup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
