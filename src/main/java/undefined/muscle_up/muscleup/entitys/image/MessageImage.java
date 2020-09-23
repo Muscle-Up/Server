75d408edb11033efa002b38afeafda8634e6fd81
Choi ha yeon <chlgky82@gmail.com>
2020-09-23 15:37:45 +0900

    [ADD] - message image entity

---
+++ src/main/java/undefined/muscle_up/muscleup/entitys/image/MessageImage.java
@@ -0,0 +1,24 @@
+package undefined.muscle_up.muscleup.entitys.image;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MessageImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Integer user_id;
+
+    private String image_name;
+}
