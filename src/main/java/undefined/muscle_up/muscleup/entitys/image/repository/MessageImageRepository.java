f240b2a9ce0d717abae24d78a42945adbcb357c4
Choi ha yeon <chlgky82@gmail.com>
2020-09-23 15:35:35 +0900

    [ADD] - message image repository

---
+++ src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/MessageImageRepository.java
@@ -0,0 +1,8 @@
+package undefined.muscle_up.muscleup.entitys.image.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageImageRepository extends CrudRepository<MessageImageRepository, Integer> {
+}
