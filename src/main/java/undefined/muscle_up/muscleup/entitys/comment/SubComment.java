fba2c4ac7657a48bf8c13ee18d40028f57365d50
Choi ha yeon <chlgky82@gmail.com>
2020-09-23 15:39:01 +0900

    [ADD] - subcomment entity

---
+++ src/main/java/undefined/muscle_up/muscleup/entitys/comment/SubComment.java
@@ -0,0 +1,31 @@
+package undefined.muscle_up.muscleup.entitys.comment;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.criteria.CriteriaBuilder;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String content;
+
+    private Integer comment_id;
+
+    private Integer user_id;
+
+    private LocalDateTime created_at;
+}
