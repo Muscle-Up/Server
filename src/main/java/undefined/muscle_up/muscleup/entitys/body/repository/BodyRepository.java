c7f793abc7eb2cdc4f4b60333c8ae45e8d9cccc2
Ryua <machiro119@naver.com>
2020-09-25 15:56:03 +0900

    [FIX] - repository generic change

--- src/main/java/undefined/muscle_up/muscleup/entitys/body/repository/BodyRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/body/repository/BodyRepository.java
@@ -2,7 +2,8 @@ package undefined.muscle_up.muscleup.entitys.body.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import undefined.muscle_up.muscleup.entitys.body.Body;
 
 @Repository
-public interface BodyRepository extends CrudRepository<BodyRepository, Integer> {
+public interface BodyRepository extends CrudRepository<Body, Integer> {
 }
