8ca26eb7b51e9281c32ed73ed2112ad161475475
Choi ha yeon <chlgky82@gmail.com>
2020-10-08 08:24:35 +0900

    [ADD] - user

--- src/main/java/undefined/muscle_up/muscleup/entitys/user/User.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/user/User.java
@@ -15,7 +15,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    public Integer id;
 
     @Column(unique = true, nullable = false)
     private String email;
