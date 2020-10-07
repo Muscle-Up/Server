a27d73395ace316926d7fbe08c0a5935bc28e2b1
Choi ha yeon <chlgky82@gmail.com>
2020-10-08 08:24:04 +0900

    [ADD] - user repository

--- src/main/java/undefined/muscle_up/muscleup/entitys/user/repository/UserRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/user/repository/UserRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.user.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findById(Integer id);
+    Optional<User> findByEmail(String email);
 }
