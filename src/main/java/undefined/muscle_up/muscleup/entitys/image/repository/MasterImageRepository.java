de7a9d4f1d79dbf72a0d5073adf308774f21bf46
Ryua <machiro119@naver.com>
2020-10-08 08:46:59 +0900

    [FIX] - add orm

--- src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/MasterImageRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/MasterImageRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.image.MasterImage;
 
+import java.util.Optional;
+
 @Repository
 public interface MasterImageRepository extends CrudRepository<MasterImage, Integer> {
+
+    Optional<MasterImage> findByImageName(String imageName);
+    Optional<MasterImage> deleteByUserId(Integer userId);
+    Optional<MasterImage> findByUserId(Integer userId);
 }
