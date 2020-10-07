9a139c00511839129c694c4881c0740ec569efd2
Choi ha yeon <chlgky82@gmail.com>
2020-10-08 08:26:40 +0900

    [ADD] - auth details service

---
+++ src/main/java/undefined/muscle_up/muscleup/security/auth/AuthDetailsService.java
@@ -0,0 +1,21 @@
+package undefined.muscle_up.muscleup.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import undefined.muscle_up.muscleup.entitys.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public AuthDetails loadUserByUsername(String receiptCode) throws UsernameNotFoundException {
+        return userRepository.findById(Integer.parseInt(receiptCode))
+                .map(AuthDetails::new)
+                .orElseThrow(RuntimeException::new);
+    }
+}
