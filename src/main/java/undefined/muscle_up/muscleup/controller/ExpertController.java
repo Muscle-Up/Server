09930d4be29fb71df01ed19d5912e0dd5e80aeb7
Ryua <machiro119@naver.com>
2020-10-08 08:46:37 +0900

    [ADD] - expert controller

--- src/main/java/undefined/muscle_up/muscleup/controller/ExpertController.java
+++ src/main/java/undefined/muscle_up/muscleup/controller/ExpertController.java
@@ -1,22 +1,51 @@
 package undefined.muscle_up.muscleup.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import undefined.muscle_up.muscleup.payload.request.ExpertRegistrationRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import undefined.muscle_up.muscleup.payload.response.PageResponse;
 import undefined.muscle_up.muscleup.service.expert.ExpertService;
 
+import javax.validation.constraints.NotBlank;
+
 @RestController
-@RequestMapping("expert")
+@RequestMapping("/expert")
 @RequiredArgsConstructor
 public class ExpertController {
 
     private final ExpertService expertService;
 
+    @GetMapping
+    public PageResponse expertList(Pageable page) {
+
+        return expertService.expertList(page);
+    }
+
     @PostMapping
-    public void registration(@RequestBody ExpertRegistrationRequest expertRegistration) {
-        expertService.registration(expertRegistration);
+    public void registration(@RequestParam @NotBlank String email,
+                             @RequestParam String introduction,
+                             MultipartFile image) {
+
+        expertService.registration(email, introduction, image);
+    }
+
+    @DeleteMapping
+    public void deleteExpert(@RequestBody String email) {
+        expertService.deleteExpert(email);
+    }
+
+    @GetMapping(
+            value = "/image/{imageName}",
+            produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE}
+    )
+    public byte[] getImage(@PathVariable String imageName) {
+        return expertService.getImage(imageName);
+    }
+
+    @PutMapping("/image")
+    public void updateImage(@RequestParam MultipartFile image) {
+        expertService.updateImage(image);
     }
 }
