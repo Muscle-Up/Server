ea3dfa336d406e5cabb7dd7bc640acc732223275
Ryua <machiro119@naver.com>
2020-10-08 08:51:18 +0900

    [ADD] - web security

--- src/main/java/undefined/muscle_up/muscleup/security/WebSecurity.java
+++ src/main/java/undefined/muscle_up/muscleup/security/WebSecurity.java
@@ -12,6 +12,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable();
+                .csrf().disable()
+                .cors();
     }
 }
