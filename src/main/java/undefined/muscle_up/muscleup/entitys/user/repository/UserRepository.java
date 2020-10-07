6538288a03944b49ac049136fbdbe75220b8abca
Ryua <machiro119@naver.com>
2020-10-08 08:49:29 +0900

    [FIX] - add orm

--- src/main/java/undefined/muscle_up/muscleup/entitys/user/repository/UserRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/user/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package undefined.muscle_up.muscleup.entitys.user.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.user.User;
+import undefined.muscle_up.muscleup.entitys.user.enums.UserType;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findAllByEmail(String email);
+    Page<User> findAllByType(UserType userType, Pageable page);
 }
