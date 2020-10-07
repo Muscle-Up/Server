56d86af9de96b86641b103aa7dd1af003ff692b5
Choi ha yeon <chlgky82@gmail.com>
2020-10-08 08:28:54 +0900

    [ADD] - user service

---
+++ src/main/java/undefined/muscle_up/muscleup/service/user/UserService.java
@@ -0,0 +1,7 @@
+package undefined.muscle_up.muscleup.service.user;
+
+import undefined.muscle_up.muscleup.payload.request.SignUpRequest;
+
+public interface UserService {
+    void signUp(SignUpRequest signUpRequest);
+}
