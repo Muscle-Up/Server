6e42ed4fc133e22ab2ab489fdaa311e2e6c48009
Choi ha yeon <chlgky82@gmail.com>
2020-10-12 17:16:36 +0900

    [FIX] - user repository

--- src/main/java/undefined/muscle_up/muscleup/entitys/user/repository/UserRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/user/repository/UserRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-    Optional<User> findById(Integer id);
     Optional<User> findByEmail(String email);
 }
