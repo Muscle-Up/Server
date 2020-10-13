ba5006aa46a7135219b3e6c3d987cac63bb6c1c6
Ryua <machiro119@naver.com>
2020-10-13 14:09:05 +0900

    [DELETE] - delete use repository orm

--- src/main/java/undefined/muscle_up/muscleup/entitys/user/repository/UserRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/user/repository/UserRepository.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-    Optional<User> findAllByEmail(String email);
+    Optional<User> findByEmail(String email);
     Page<User> findAllByType(UserType userType, Pageable page);
 }
