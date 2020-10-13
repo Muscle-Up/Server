dbaedfe9f2184e46f651d0fcebbbb71e6c1867c1
Choi ha yeon <chlgky82@gmail.com>
2020-10-13 18:37:32 +0900

    [FIX] - user service impl

--- src/main/java/undefined/muscle_up/muscleup/service/user/UserServiceImpl.java
+++ src/main/java/undefined/muscle_up/muscleup/service/user/UserServiceImpl.java
@@ -43,8 +43,8 @@ public class UserServiceImpl implements UserService {
                 .age(signUpRequest.getAge())
                 .sex(signUpRequest.getSex())
                 .type(UserType.USER)
-                .heigth(signUpRequest.getHeigth())
-                .weigth(signUpRequest.getWeigth())
+                .height(signUpRequest.getHeigth())
+                .weight(signUpRequest.getWeigth())
                 .build()
         );
 
