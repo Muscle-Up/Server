73199d9d4f23a9bc46ae34f901cac025ef0036e3
Hanif <marbling1293@naver.com>
2020-10-15 17:01:44 +0900

    [ADD] Subcomment Repository

--- src/main/java/undefined/muscle_up/muscleup/entitys/comment/repository/SubCommentRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/comment/repository/SubCommentRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.comment.SubComment;
 
+import java.util.List;
+
 @Repository
 public interface SubCommentRepository extends CrudRepository<SubComment, Integer> {
+    List<SubComment> findAllByCommentIdOrderByCommentIdAsc(Integer commendId);
+
+    void deleteById(Integer commentId);
 }
