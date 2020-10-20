772e869487106ececd5af0844b7a222557fb7c15
Hanif <marbling1293@naver.com>
2020-10-20 10:23:14 +0900

    [ADD] QnaBoardLikeRepository 메서드 추가

--- src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/repository/QnaBoardLikeRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/repository/QnaBoardLikeRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.qnaboard.QnaBoardLike;
 
+import java.util.Optional;
+
 @Repository
 public interface QnaBoardLikeRepository extends CrudRepository<QnaBoardLike, Integer> {
+
+    Optional<QnaBoardLike> findByBoardIdAndUserId(Integer boardId, Integer userId);
+
+    void deleteByBoardId(Integer boardId);
 }
