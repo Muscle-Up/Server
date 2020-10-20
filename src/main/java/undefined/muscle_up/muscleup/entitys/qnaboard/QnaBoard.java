2dfa05dd5a7dce1023c5ae50e2e09911f457726e
Hanif <marbling1293@naver.com>
2020-10-20 10:23:38 +0900

    [ADD] QnaBoard Entitiy

--- src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/QnaBoard.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/QnaBoard.java
@@ -23,10 +23,33 @@ public class QnaBoard {
 
     private String content;
 
+    private Integer userId;
+
     private Integer view;
 
     private Integer likeCount;
 
     private LocalDateTime createdAt;
 
+    public QnaBoard view() {
+        view++;
+        return this;
+    }
+
+    public QnaBoard like() {
+        likeCount++;
+        return this;
+    }
+
+    public QnaBoard unLike() {
+        if(likeCount >= 1) likeCount--;
+        return this;
+    }
+
+    public QnaBoard updateBoard(String title, String content) {
+        this.title = title;
+        this.content = content;
+
+        return this;
+    }
 }
