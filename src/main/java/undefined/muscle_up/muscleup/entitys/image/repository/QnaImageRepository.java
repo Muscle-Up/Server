7ca4561ea7679ba4888bae48aef3db0c7265f6a6
Hanif <marbling1293@naver.com>
2020-10-20 10:21:59 +0900

    [ADD] QnaImageRepository 메서드 추가

--- src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/QnaImageRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/QnaImageRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.image.QnaImage;
 
+import java.util.List;
+
 @Repository
 public interface QnaImageRepository extends CrudRepository<QnaImage, Integer> {
+
+    List<QnaImage> findByBoardIdOrderById(Integer boardId);
+    void deleteByBoardId(Integer boardId);
 }
