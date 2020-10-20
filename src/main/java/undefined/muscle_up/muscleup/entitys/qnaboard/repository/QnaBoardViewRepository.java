a0f855b06917c7c657fe99649675e44ee1619127
Hanif <marbling1293@naver.com>
2020-10-20 10:22:56 +0900

    [ADD] QnaBoardViewRepository 메서드 추가

--- src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/repository/QnaBoardViewRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/repository/QnaBoardViewRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.qnaboard.QnaBoardView;
 
+import java.util.Optional;
+
 @Repository
 public interface QnaBoardViewRepository extends CrudRepository<QnaBoardView, Integer> {
+
+    Optional<QnaBoardView> findByBoardIdAndUserId(Integer boardId, Integer userId);
 }
