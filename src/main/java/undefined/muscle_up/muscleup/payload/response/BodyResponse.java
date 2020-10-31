774d40549e9fa77b0af462a6523a87f2502462da
Seogunhee <seogunhee4@gmail.com>
2020-11-01 00:31:37 +0900

    [ADD]BodyResponse추가

---
+++ src/main/java/undefined/muscle_up/muscleup/payload/response/BodyResponse.java
@@ -0,0 +1,21 @@
+package undefined.muscle_up.muscleup.payload.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class BodyResponse {
+
+    private Integer bodyId;
+
+    private String title;
+
+    private String content;
+
+    private LocalDate createdAt;
+
+
+}
