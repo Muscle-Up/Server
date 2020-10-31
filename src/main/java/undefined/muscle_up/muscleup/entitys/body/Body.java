a456fe31a5ef1fe27bd5932b07816b6373a6b60f
Seogunhee <seogunhee4@gmail.com>
2020-11-01 00:28:51 +0900

    [FIX]Body와 BodyRepository수정

--- src/main/java/undefined/muscle_up/muscleup/entitys/body/Body.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/body/Body.java
@@ -2,10 +2,7 @@ package undefined.muscle_up.muscleup.entitys.body;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
@@ -19,12 +16,22 @@ public class Body {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column
     private Integer userId;
 
+    @Column
     private String title;
 
+    @Column
     private String content;
 
+    @Column
     private LocalDate createdAt;
 
+    public Body update(String title, String content) {
+        this.title = title;
+        this.content = content;
+
+        return this;
+    }
 }
--- src/main/java/undefined/muscle_up/muscleup/entitys/body/repository/BodyRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/body/repository/BodyRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.body.Body;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BodyRepository extends CrudRepository<Body, Integer> {
+    Optional<Body> findByUserId(Integer userId);
+    Optional<Body> findById(Integer id);
+    void deleteById(Integer id);
+    List<Body> findAllByUserId(Integer userID);
 }
+
