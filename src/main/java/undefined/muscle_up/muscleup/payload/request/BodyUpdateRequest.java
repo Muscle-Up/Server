ab3de484a6f978423322ab3f875c8305cd64c610
Seogunhee <seogunhee4@gmail.com>
2020-11-03 08:23:22 +0900

    [ADD]BodyUpdateRequest추가

---
+++ src/main/java/undefined/muscle_up/muscleup/payload/request/BodyUpdateRequest.java
@@ -0,0 +1,12 @@
+package undefined.muscle_up.muscleup.payload.request;
+
+import lombok.Getter;
+
+@Getter
+public class BodyUpdateRequest {
+
+    private String title;
+
+    private String content;
+
+}
