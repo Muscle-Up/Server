24686764c00decf0692024f2c81f43b4bca24060
Choi ha yeon <chlgky82@gmail.com>
2020-11-16 15:41:23 +0900

    [FIX] - user image repository

--- src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/UserImageRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/UserImageRepository.java
@@ -2,7 +2,6 @@ package undefined.muscle_up.muscleup.entitys.image.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import undefined.muscle_up.muscleup.entitys.image.MasterImage;
 import undefined.muscle_up.muscleup.entitys.image.UserImage;
 
 import java.util.Optional;
