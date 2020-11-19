5c85ff41f76ac99e26ede8d6e6e2f246d48930f5
Seogunhee <seogunhee4@gmail.com>
2020-11-19 23:15:37 +0900

    [FIX]GraphResponse수정

--- src/main/java/undefined/muscle_up/muscleup/payload/response/GraphResponse.java
+++ src/main/java/undefined/muscle_up/muscleup/payload/response/GraphResponse.java
@@ -7,9 +7,6 @@ import lombok.Getter;
 @Builder
 public class GraphResponse {
     private Integer id;
-    private double tall;
-    private double weight;
-    private double muscular_strength;
-    private double fat_percentage;
+    private double value;
 }
 
