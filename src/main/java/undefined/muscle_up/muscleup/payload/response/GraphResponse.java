cc79845960e55347abcc09f22a211e9b3ac0adb6
Seogunhee <seogunhee4@gmail.com>
2020-11-20 20:10:34 +0900

    [FIX]GraphRepository수정 날짜 추가

--- src/main/java/undefined/muscle_up/muscleup/payload/response/GraphResponse.java
+++ src/main/java/undefined/muscle_up/muscleup/payload/response/GraphResponse.java
@@ -3,10 +3,13 @@ package undefined.muscle_up.muscleup.payload.response;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @Getter
 @Builder
 public class GraphResponse {
     private Integer id;
     private double value;
+    private LocalDate createAt;
 }
 
