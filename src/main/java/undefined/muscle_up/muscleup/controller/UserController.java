4deb68ec2425ba28ded46ed0588198756b8d4980
Ryua <machiro119@naver.com>
2020-12-02 19:03:19 +0900

    [FIX] - delete import

--- src/main/java/undefined/muscle_up/muscleup/controller/UserController.java
+++ src/main/java/undefined/muscle_up/muscleup/controller/UserController.java
@@ -1,13 +1,9 @@
 package undefined.muscle_up.muscleup.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import undefined.muscle_up.muscleup.entitys.user.enums.Sex;
-import undefined.muscle_up.muscleup.entitys.user.enums.UserType;
 import undefined.muscle_up.muscleup.payload.request.SignUpRequest;
 import undefined.muscle_up.muscleup.service.user.UserService;
 
@@ -43,5 +39,6 @@ public class UserController {
                         .build()
         );
     }
+
 
 }
