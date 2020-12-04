e25c59f211c2865a0090393ac36a054c8ccfe741
Seogunhee <seogunhee4@gmail.com>
2020-12-04 16:25:44 +0900

    [FIX]my mistake

--- src/main/java/undefined/muscle_up/muscleup/service/graph/GraphServiceImpl.java
+++ src/main/java/undefined/muscle_up/muscleup/service/graph/GraphServiceImpl.java
@@ -3,15 +3,18 @@ package undefined.muscle_up.muscleup.service.graph;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import undefined.muscle_up.muscleup.entitys.graph.Graph;
+import undefined.muscle_up.muscleup.entitys.graph.enums.GraphType;
 import undefined.muscle_up.muscleup.entitys.graph.repository.GraphRepository;
 import undefined.muscle_up.muscleup.entitys.user.repository.UserRepository;
 import undefined.muscle_up.muscleup.payload.request.GraphRequest;
 import undefined.muscle_up.muscleup.payload.response.GraphResponse;
 import undefined.muscle_up.muscleup.security.auth.AuthenticationFacade;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
+
 
 @Service
 @RequiredArgsConstructor
@@ -22,11 +25,56 @@ public class GraphServiceImpl implements GraphService{
     private final AuthenticationFacade authenticationFacade;
 
     private <T> void setIfNotNull(Consumer<T> setter, T value) {
-        if(value.equals(0.0)) {
-            System.out.println(value);
-            return;
+        if(!value.equals(0.0)) {
+            setter.accept(value);
         }
-        setter.accept(value);
+    }
+
+    private double getCheckAndValue(GraphType type, Graph graph) {
+        if(type.equals(GraphType.muscleMass))
+            return graph.getMuscleMass();
+        else if(type.equals(GraphType.bodyFatMass))
+            return graph.getBodyFatMass();
+        else if(type.equals(GraphType.weight))
+            return graph.getWeight();
+        else
+            throw new RuntimeException();
+    }
+
+    @Override
+    public boolean checkGraphToday() {
+        Integer receiptCode = authenticationFacade.getReceiptCode();
+        userRepository.findById(receiptCode)
+                .orElseThrow(RuntimeException::new);
+
+        List<Graph> graphList = graphRepository.findAllByUserId(receiptCode);
+
+        for(Graph graph : graphList) {
+            if(graph.getCreateAt().isEqual(LocalDate.now()))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<GraphResponse> getGraph(GraphType graphType) {
+        Integer receiptCode = authenticationFacade.getReceiptCode();
+        userRepository.findById(receiptCode)
+                .orElseThrow(RuntimeException::new);
+
+        List<Graph> graphs = graphRepository.findAllByUserId(receiptCode);
+        List<GraphResponse> responses = new ArrayList<>();
+
+        for(Graph graph : graphs) {
+            double value = getCheckAndValue(graphType, graph);
+            GraphResponse graphResponse = GraphResponse.builder()
+                    .id(graph.getId())
+                    .value(value)
+                    .createAt(graph.getCreateAt())
+                    .build();
+            responses.add(graphResponse);
+        }
+        return responses;
     }
 
     @Override
@@ -37,38 +85,29 @@ public class GraphServiceImpl implements GraphService{
 
         graphRepository.save(
                 Graph.builder()
-                        .tall(graphCreateRequest.getTall())
-                        .weight(graphCreateRequest.getWeight())
-                        .muscular_strength(graphCreateRequest.getMuscular_strength())
-                        .fat_percentage(graphCreateRequest.getFat_percentage())
-                        .userId(receiptCode)
-                        .build()
+                .userId(receiptCode)
+                .weight(graphCreateRequest.getWeight())
+                .bodyFatMass(graphCreateRequest.getBodyFatMass())
+                .muscleMass(graphCreateRequest.getMuscleMass())
+                .createAt(LocalDate.now())
+                .build()
         );
     }
 
     @Override
-    public List<GraphResponse> getGraph() {
+    public void updateGraph(GraphRequest graphUpdateRequest, Integer graphId) {
         Integer receiptCode = authenticationFacade.getReceiptCode();
         userRepository.findById(receiptCode)
                 .orElseThrow(RuntimeException::new);
 
-        List<Graph> graphs = graphRepository.findAllByUserId(receiptCode);
-        List<GraphResponse> responses = new ArrayList<>();
+        Graph graph = graphRepository.findById(graphId)
+                .orElseThrow(RuntimeException::new);
 
-        for(Graph graph : graphs) {
-            GraphResponse graphResponse =
-                    GraphResponse.builder()
-                            .id(graph.getId())
-                            .tall(graph.getTall())
-                            .weight(graph.getWeight())
-                            .fat_percentage(graph.getFat_percentage())
-                            .muscular_strength(graph.getMuscular_strength())
-                            .build();
+        setIfNotNull(graph::setMuscleMass, graphUpdateRequest.getMuscleMass());
+        setIfNotNull(graph::setBodyFatMass, graphUpdateRequest.getBodyFatMass());
+        setIfNotNull(graph::setWeight, graphUpdateRequest.getWeight());
 
-            responses.add(graphResponse);
-        }
-
-        return responses;
+        graphRepository.save(graph);
     }
 
     @Override
@@ -81,23 +120,6 @@ public class GraphServiceImpl implements GraphService{
                 .orElseThrow(RuntimeException::new);
 
         graphRepository.deleteById(graph.getId());
-    }
-
-    @Override
-    public void updateGraph(GraphRequest graphUpdateRequest, Integer graphId) {
-        Integer receiptCode = authenticationFacade.getReceiptCode();
-        userRepository.findById(receiptCode)
-                .orElseThrow(RuntimeException::new);
-
-        Graph graph = graphRepository.findById(graphId)
-                .orElseThrow(RuntimeException::new);
-
-        setIfNotNull(graph::setTall, graphUpdateRequest.getTall());
-        setIfNotNull(graph::setWeight, graphUpdateRequest.getWeight());
-        setIfNotNull(graph::setMuscular_strength, graphUpdateRequest.getMuscular_strength());
-        setIfNotNull(graph::setFat_percentage, graphUpdateRequest.getFat_percentage());
-
-        graphRepository.save(graph);
     }
 }
 
