b084927f5e79a62ea2f43e36e675ae95b03e8bb4
Ryua <machiro119@naver.com>
2020-12-18 11:31:55 +0900

    [ADD] - error code

---
+++ src/main/java/undefined/muscle_up/muscleup/error/excepion/ErrorCode.java
@@ -0,0 +1,19 @@
+package undefined.muscle_up.muscleup.error.excepion;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum  ErrorCode {
+
+    INVALID_TOKEN(401,"Invalid Token"),
+    EXPIRED_TOKEN(401,"Expired Token"),
+    PERMISSION_DENIED_EXCEPTION(401,"Permission Denied"),
+    USER_NOT_FOUND(404,"User Not Found"),
+    IMAGE_NOT_FOUND(404, "Image Not Found");
+
+    private final int status;
+    private final String message;
+
+}
