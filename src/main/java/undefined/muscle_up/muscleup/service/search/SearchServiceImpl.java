68c6f764ee48724cbd1b3ac886ac497812ac0e00
Ryua <machiro119@naver.com>
2020-12-25 02:02:05 +0900

    [FIX] - change run time exception

--- src/main/java/undefined/muscle_up/muscleup/service/search/SearchServiceImpl.java
+++ src/main/java/undefined/muscle_up/muscleup/service/search/SearchServiceImpl.java
@@ -6,6 +6,7 @@ import undefined.muscle_up.muscleup.entitys.image.repository.MasterImageReposito
 import undefined.muscle_up.muscleup.entitys.image.repository.UserImageRepository;
 import undefined.muscle_up.muscleup.entitys.user.User;
 import undefined.muscle_up.muscleup.entitys.user.repository.UserRepository;
+import undefined.muscle_up.muscleup.exceptions.UserNotFoundException;
 import undefined.muscle_up.muscleup.payload.response.ExpertResponse;
 import undefined.muscle_up.muscleup.security.auth.AuthenticationFacade;
 
@@ -17,16 +18,13 @@ import java.util.List;
 public class SearchServiceImpl implements SearchService{
 
     private final UserRepository userRepository;
-    private final UserImageRepository userImageRepository;
-    private final MasterImageRepository masterImageRepository;
 
     private final AuthenticationFacade authenticationFacade;
 
     @Override
     public List<ExpertResponse> searchExpert(String certificateName) {
-        Integer receiptCode = authenticationFacade.getId();
-        userRepository.findById(receiptCode)
-                .orElseThrow(RuntimeException::new);
+        userRepository.findById(authenticationFacade.getId())
+                .orElseThrow(UserNotFoundException::new);
 
         List<User> target = userRepository.findByCertificateNameContains(certificateName);
 
