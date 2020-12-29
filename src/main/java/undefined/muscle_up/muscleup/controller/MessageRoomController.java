5aaab5d0caa0137e16e8d9a2db5ee39e31bd301f
Choi ha yeon <chlgky82@gmail.com>
2020-12-29 22:15:26 +0900

    [FIX] - message controller

--- src/main/java/undefined/muscle_up/muscleup/controller/MessageRoomController.java
+++ src/main/java/undefined/muscle_up/muscleup/controller/MessageRoomController.java
@@ -2,7 +2,9 @@ package undefined.muscle_up.muscleup.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import undefined.muscle_up.muscleup.payload.response.MessageResponse;
 import undefined.muscle_up.muscleup.payload.response.MessageRoomListResponse;
+import undefined.muscle_up.muscleup.service.message.MessageService;
 import undefined.muscle_up.muscleup.service.message_room.MessageRoomService;
 
 import java.util.List;
@@ -13,14 +15,22 @@ import java.util.List;
 public class MessageRoomController {
 
     private final MessageRoomService messageRoomService;
+    private final MessageService messageService;
+
+    @GetMapping
+    public List<MessageRoomListResponse> getMessageRoomList(){
+        return messageRoomService.getMessageRoomList();
+    }
+
+    @GetMapping("/{roomId}")
+    public List<MessageResponse> getMessageList(@PathVariable String roomId){
+        return messageService.getMessageList(roomId);
+    }
 
     @PostMapping("/{targetId}")
     public void createMessageRoom(@PathVariable Integer targetId){
         messageRoomService.createMessageRoom(targetId);
     }
 
-    @GetMapping
-    public List<MessageRoomListResponse> getMessageRoomList(){
-        return messageRoomService.getMessageRoomList();
-    }
+
 }
