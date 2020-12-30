4beb29455c4cd1be656d095c83695fb9a0e8f454
Seogunhee <seogunhee4@gmail.com>
2020-12-30 15:31:32 +0900

    [FIX]repository수정

--- src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/BodyImageRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/image/repository/BodyImageRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.image.BodyImage;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BodyImageRepository extends CrudRepository<BodyImage, Integer> {
     Optional<BodyImage> findByBodyId(Integer bodyId);
+    Optional<BodyImage> findByImageName(String imageName);
+    List<BodyImage> findAllByBodyId(Integer bodyId);
     void deleteById(Integer id);
 }
