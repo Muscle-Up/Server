3a3250f734128cc69d4efbac6e450c68be9f5ff1
Ryua <machiro119@naver.com>
2021-01-06 08:07:26 +0900

    [ADD] - qna board list query

--- src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/repository/QnaBoardRepository.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/repository/QnaBoardRepository.java
@@ -1,6 +1,7 @@
 package undefined.muscle_up.muscleup.entitys.qnaboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import undefined.muscle_up.muscleup.entitys.qnaboard.QnaBoard;
 
@@ -10,6 +11,8 @@ import java.util.Optional;
 @Repository
 public interface QnaBoardRepository extends JpaRepository<QnaBoard, Integer> {
     Optional<QnaBoard> findById(Integer boardId);
-    List<QnaBoard> findAllByOrderByCreatedAtDesc();
     void deleteById(Integer boardId);
+
+    @Query(value = "select qna.* from qna_board as qna join ban_user as ban on ban.blocker_people = :userId where qna.user_id != ban.block_out_people limit :startIndex, :endIndex", nativeQuery = true)
+    List<QnaBoard> findAllBoardsWithoutBlockOut(Integer userId, Integer startIndex, Integer endIndex);
 }
