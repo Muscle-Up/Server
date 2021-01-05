12a92a61e168f7196eda86c5a2c9ddb61e389c6c
Ryua <machiro119@naver.com>
2021-01-06 08:06:56 +0900

    [ADD] - user ban import

--- src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/QnaBoard.java
+++ src/main/java/undefined/muscle_up/muscleup/entitys/qnaboard/QnaBoard.java
@@ -1,11 +1,9 @@
 package undefined.muscle_up.muscleup.entitys.qnaboard;
 
 import lombok.*;
+import undefined.muscle_up.muscleup.entitys.ban.BanUser;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
